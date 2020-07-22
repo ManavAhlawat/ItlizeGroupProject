cec59cc00ace7e0f883d0c1c633ed971edadcdd3
Manav Ahlawat <manavahlawat95@gmail.com>
2020-07-21 21:56:30 -0400

    Adding project structure

---
+++ src/main/java/com/Project1/ItlizeGroupProject/ItlizeGroupProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Project1.ItlizeGroupProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItlizeGroupProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItlizeGroupProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Project1/ItlizeGroupProject/ItlizeGroupProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Project1.ItlizeGroupProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItlizeGroupProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
