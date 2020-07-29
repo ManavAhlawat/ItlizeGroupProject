6d9bb69c8344038d4f9871bb0d306359f2386deb
Manav Ahlawat <manavahlawat95@gmail.com>
2020-07-29 10:27:07 -0400

    Authentication working with hardcoded user

--- src/main/java/com/Project1/ItlizeGroupProject/Models/AuthenticationResponse.java
+++ src/main/java/com/Project1/ItlizeGroupProject/Models/AuthenticationResponse.java
@@ -1,16 +1,13 @@
-//package com.Project1.ItlizeGroupProject.Models;
-//
-//import java.io.Serializable;
-//
-//public class AuthenticationResponse implements Serializable {
-//
-//    private final String jwt;
-//
-//    public AuthenticationResponse(String jwt) {
-//        this.jwt = jwt;
-//    }
-//
-//    public String getJwt() {
-//        return jwt;
-//    }
-//}
+package com.Project1.ItlizeGroupProject.Models;
+
+import java.io.Serializable;
+public class AuthenticationResponse implements Serializable {
+    private static final long serialVersionUID = -8091879091924046844L;
+    private final String jwttoken;
+    public AuthenticationResponse(String jwttoken) {
+        this.jwttoken = jwttoken;
+    }
+    public String getToken() {
+        return this.jwttoken;
+    }
+}
